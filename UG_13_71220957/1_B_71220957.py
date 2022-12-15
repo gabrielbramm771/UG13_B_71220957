e77018d7f01871184dc5c483aa973b4425a17b33
gabrielbramm771 <117884203+gabrielbramm771@users.noreply.github.com>
2022-12-15 12:58:53 +0700

    Add files via upload

---
+++ UG_13_71220957/1_B_71220957.py
@@ -0,0 +1,79 @@
+# import turtle
+# t = turtle.Turtle()
+# t.reset()
+# t.pencolor("White")
+# t.pensize(5)
+# t.penup()
+# t.goto(-300,200)
+
+# #G
+# t.pendown()
+# t.
+
+# G
+import turtle as t
+t.pensize(10)
+t.color("Black")
+t.circle(-80,-180)
+t.right(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.penup()
+t.backward(110)
+
+
+# 9
+import turtle as t
+t.pensize(10)
+t.color("black")
+t.goto(20,5)
+t.pendown()
+t.backward(80)
+t.right(90)
+t.backward(80)
+t.right(90)
+t.backward(80)
+t.penup()
+t.goto(20,5)
+t.pendown()
+t.left(90)
+t.goto(20,5)
+t.backward(150)
+t.right(90)
+t.forward(100)
+t.penup()
+t.forward(110)
+
+# 5
+t.pensize(10)
+t.color("black")
+t.pendown()
+t.goto(20,20)
+# t.backward(100)
+# t.right(100)
+# t.forward(100) 
+# t.left(100)
+# t.backward(100)
+
+# 7
+
+
+# # A
+# import turtle  as t
+# t.pensize(10)
+# t.color("Black")
+# t.penup()
+# t.forward(20)
+# t.pendown()
+# t.forward(130)
+# t.right(45)
+# t.forward(50)
+# t.backward(150)
+# t.right(90)
+# t.forward(150)
+# t.pendown()
+
+
+
+
